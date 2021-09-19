0c078e3d9d98892a09f9b52aeb0e27b6285d5c13
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-19 16:25:15 +0900

    [우선순위 큐] 9월 19일 -ing
    11000번_시간초과

---
+++ 9월 14일 - 우선순위 큐/11000.cpp
@@ -0,0 +1,60 @@
+//
+// Created by 이유정 on 2021/09/19.
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+vector<int> v;
+
+int main() {
+
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, s, t;
+    cin >> N;
+
+    while (N--) {
+
+        cin >> s >> t;
+
+        pq.push({s, t});
+
+    }
+
+
+    v.push_back(0); //0
+
+
+    while (!pq.empty()) {
+        int start = pq.top().first; //수업 시작 시간
+        int end = pq.top().second;
+        int flag = false;
+        pq.pop();
+
+        for (int i = 0; i < v.size(); i++) {
+
+            if (v[i] <= start) {
+                v[i] = end;
+                flag = true;
+                break;
+            }
+
+
+        }
+
+        if (flag == false) {
+            v.push_back(end);
+        }
+
+    }
+
+    cout << v.size();
+
+}
