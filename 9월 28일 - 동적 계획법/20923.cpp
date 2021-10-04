430355742336c3fc0ee8b3b7958fd987254eb54a
YujeongLEE <dldbwjd24@ewhain.net>
2021-10-04 16:02:57 +0900

    [동적 계획법] 10월 4일 - Update
    20923번_수정

--- 9월 28일 - 동적 계획법/20923.cpp
+++ 9월 28일 - 동적 계획법/20923.cpp
@@ -22,66 +22,80 @@ using namespace std;
 deque<int> dodo;
 deque<int> suy;
 
-stack<int> do_gnd;
-stack<int> su_gnd;
-
+deque<int> do_gnd;
+deque<int> su_gnd;
+int winner;
 
 bool check_su() {//도도의 그라운드top, 수연의 ground top
     //2 . 수연 체크
     if (!do_gnd.empty() && !su_gnd.empty()) { //둘다 empty아닐 떄만 가능
-        return do_gnd.top() + su_gnd.top() == 5;
+        return do_gnd.back() + su_gnd.back() == 5;
     }
     return false;
 }
 
-bool check_dodo() {
+bool check_dodo(int card) {
 
-    if (do_gnd.empty() && su_gnd.empty()) return false;//둘다 빔
-
-    //둘중 하나만 빔
-
-    if (do_gnd.empty()) {
-        return su_gnd.top() == 5;
-    }
-
-    if (su_gnd.empty()) {
-        return do_gnd.top() == 5;
-    }
-
-    // 둘다 안빔
-    return (do_gnd.top() == 5 || su_gnd.top() == 5);
-
-}
-
-
-void dodoWin() {
-
-    while (!su_gnd.empty()) {
-        dodo.push_front(su_gnd.top());
-        su_gnd.pop();
-    }
-
-    while (!do_gnd.empty()) {
-        dodo.push_front(do_gnd.top());
-        do_gnd.pop();
-    }
+    return card == 5;
+//    if (do_gnd.empty() && su_gnd.empty()) return false;//둘다 빔
+//
+//    //둘중 하나만 빔
+//
+//    if (do_gnd.empty()) {
+//        return su_gnd.top() == 5;
+//    }
+//
+//    if (su_gnd.empty()) {
+//        return do_gnd.top() == 5;
+//    }
+//
+//    // 둘다 안빔
+//    return (do_gnd.top() == 5 || su_gnd.top() == 5);
 
 }
 
-void suWin() {
 
+void win() {
 
-    while (!do_gnd.empty()) {
-        suy.push_front(do_gnd.top());
-        do_gnd.pop();
+    if (winner == 1) { //dodo win
+        while (!su_gnd.empty()) {
+            dodo.push_front(su_gnd.front());
+            su_gnd.pop_front();
+        }
+
+        while (!do_gnd.empty()) {
+            dodo.push_front(do_gnd.front());
+            do_gnd.pop_front();
+        }
+    } else if (winner == 2) { //수연 win
+        while (!do_gnd.empty()) {
+            suy.push_front(do_gnd.front());
+            do_gnd.pop_front();
+        }
+
+        while (!su_gnd.empty()) {
+            suy.push_front(su_gnd.front());
+            su_gnd.pop_front();
+        }
     }
-
-    while (!su_gnd.empty()) {
-        suy.push_front(su_gnd.top());
-        su_gnd.pop();
-    }
+    winner = 0;
 
 }
+
+//void suWin() {
+//
+//
+//    while (!do_gnd.empty()) {
+//        suy.push_front(do_gnd.front());
+//        do_gnd.pop_front();
+//    }
+//
+//    while (!su_gnd.empty()) {
+//        suy.push_front(su_gnd.front());
+//        su_gnd.pop_front();
+//    }
+//
+//}
 
 bool emptyCheck() {
 
@@ -119,40 +133,39 @@ int main() {
 
     while (true) {
 
-//        cout << "===================[ " << m << " ]=======================\n";
-
         //도도
 
         // 카드 내려두기
-        if (emptyCheck()) break;
-        do_gnd.push(dodo.back());
+        do_gnd.push_back(dodo.back());
         dodo.pop_back();
         if (emptyCheck()) break;
 
         //종을 칠 수 있는가? -> 칠 수 있으면 치기
-        do_res = check_dodo();
-        if (do_res) dodoWin();
-        else {
-            su_res = check_su();
-            if (su_res) suWin();
-        }
+        do_res = check_dodo(do_gnd.back());
+        su_res = check_su();
+
+
+        if (do_res) winner = 1;
+        else if (su_res) winner = 2;
+        win();
+
         m--;
         if (m <= 0) break;
 
 
         //수연 카드 내려두기
-        if (emptyCheck()) break;
-        su_gnd.push(suy.back());
+        su_gnd.push_back(suy.back());
         suy.pop_back();
         if (emptyCheck()) break;
 
         //종을 칠 수 있는가? -> 칠 수 있으면 치기
-        do_res = check_dodo();
-        if (do_res) dodoWin();
-        else {
-            su_res = check_su();
-            if (su_res) suWin();
-        }
+        do_res = check_dodo(su_gnd.back());
+        su_res = check_su();
+
+        if (do_res) winner = 1;
+        else if (su_res) winner = 2;
+        win();
+
         m--;
         if (m <= 0) break;
 
