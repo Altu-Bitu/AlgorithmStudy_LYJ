5e4812fc35f918942f89f3be9505a174efce625a
YujeongLEE <dldbwjd24@ewhain.net>
2021-10-07 11:26:54 +0900

    [그리디 알고리즘] 10월 7일 - ing
    프로그래머스 - 큰 수 만들기 _ 생각한게 틀렸다

--- 10월 1일 - 그리디 알고리즘/maxNumber.cpp
+++ 10월 1일 - 그리디 알고리즘/maxNumber.cpp
@@ -1,4 +1,62 @@
 //
-// Created by 이유정 on 2021/10/07.
+// 큰 수 만들기
 //
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+map<int, int> m;
+
+//
+bool compare(const pair<int, int> &a, const pair<int, int> &b) {
+    if (a.second == b.second) return a.first < b.first;
+    return a.second < b.second;
+}
+
+void makeMap(string s) {
+    for (int i = 1; i <= s.size(); i++) {
+        int a = s.at(i - 1) - '0';
+        m.insert(make_pair(i, a));
+    }
+
+//    for (auto it = m.begin(); it != m.end(); it++) {
+//        cout << it->first << " " << it->second << "\n";
+//    }
+}
+
+
+string solution(string number, int k) {
+    string answer = "";
+
+    makeMap(number);
+    vector<pair<int, int>> vec(m.begin(), m.end());
+    sort(vec.begin(), vec.end(), compare);
+
+    for (int i = 0; i < k; i++) {
+        m.erase(vec[i].first);
+    }
+    for (auto it = m.begin(); it != m.end(); it++) {
+        string s = to_string(it->second);
+        answer.append(s);
+    }
+
+//    cout << answer;
+
+    return answer;
+}
+
+int main() {
+
+    string number;
+    cin >> number;
+
+    int k;
+    cin >> k;
+
+
+    solution(number, k);
+}
