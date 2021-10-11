b7597ef32af4b0299daa5b4477553a6d3471add3
YujeongLEE <dldbwjd24@ewhain.net>
2021-10-11 11:49:58 +0900

    [분할 정복] 10월 11일 - ing
    21314번

---
+++ 10월 5일 - 분할 정복/21314.cpp
@@ -0,0 +1,106 @@
+//
+// 민겸수
+//
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+queue<char> q;
+string max_str;
+string min_str;
+
+int main() {
+
+    string mk;
+
+    cin >> mk;
+
+    for (int i = 0; i < mk.length(); i++) {
+        q.push(mk[i]);
+    }
+
+    //find max
+    //K가 나올 때 마다 변환
+    char c;
+    int num = 0;
+    while (!q.empty()) {
+
+        c = q.front();
+        q.pop();
+        if (c == 'K') {
+
+            max_str += '5';
+            if (num > 0) { //쌓인 M의 수 만큼 0붙이기
+                for (int i = 0; i < num; i++) {
+                    max_str += '0';
+                }
+            }
+            num = 0;
+            continue;
+        }
+
+        num++;//m의 수
+
+
+    }
+
+    //남은 것 출력
+    if (num == 1) {
+        max_str += '1';
+    } else if (num > 0) {
+        max_str += '1';
+        for (int i = 0; i < num - 1; i++) { //1이 맨 앞에 붙어서 1개 적은 0의 수
+            max_str += '0';
+        }
+    }
+
+
+    //find min
+    for (int i = 0; i < mk.length(); i++) {
+        q.push(mk[i]);
+    }
+
+    num = 0;
+    while (!q.empty()) {
+
+        c = q.front();
+        q.pop();
+        if (c == 'K') {
+
+            //모아둔 M출력
+            if (num == 1) {
+                min_str += '1';
+            } else if (num > 0) {
+                min_str += '1';
+                for (int i = 0; i < num - 1; i++) {
+                    min_str += '0';
+                }
+            }
+            num = 0;
+
+            //K붙이기
+            min_str += '5';
+            continue;
+        }
+
+        num++;//m의 수
+
+
+    }
+
+    //남은 M출력
+    if (num == 1) {
+        min_str += '1';
+    } else if (num > 0) {
+        min_str += '1';
+        for (int i = 0; i < num - 1; i++) {
+            min_str += '0';
+        }
+    }
+
+
+    cout << max_str << "\n" << min_str;
+}
+
