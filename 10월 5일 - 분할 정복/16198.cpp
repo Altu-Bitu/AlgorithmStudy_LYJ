394d903f66b415d2fa6b19a2b7688a64379e8587
YujeongLEE <dldbwjd24@ewhain.net>
2021-10-11 11:06:52 +0900

    [분할 정복] 10월 11일 - ing
    16198번

---
+++ 10월 5일 - 분할 정복/16198.cpp
@@ -0,0 +1,92 @@
+//
+// 에너지 모으기
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> balls;
+vector<bool> check;
+vector<int> num_list;
+queue<int> q;
+long long max_num;
+
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    balls.assign(n, 0);
+    num_list.assign(n - 2, 0);
+    check.assign(n, false);
+
+    int k = 0;
+    for (int i = 1; i < n - 1; i++) {
+        num_list[k++] = i;
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        cin >> balls[i];
+    }
+
+
+    do {
+        check[0] = true;
+        check[n - 1] = true;
+
+        for (int i = 0; i < num_list.size(); i++) {
+            q.push(num_list[i]);
+        }
+
+        long long sum = 0;
+        while (!q.empty()) {
+
+            int idx = q.front();
+            q.pop();
+
+            //사용한 것 체크
+            check[idx] = true;
+
+
+            //안사용한 부분찾기 
+            int left = idx - 1;
+            int right = idx + 1;
+
+            while (left != 0) {
+                if (check[left]) {
+                    left--;
+                } else {
+                    break;
+                }
+            }
+
+            while (right != n - 1) {
+                if (!check[right]) {
+                    break;
+                } else {
+                    right++;
+                }
+            }
+            sum += balls[left] * balls[right];
+
+        }
+
+        //최대값 갱신 + 초기화
+        if (max_num < sum) max_num = sum;
+        sum = 0;
+        fill(check.begin(), check.end(), false);
+
+
+    } while (next_permutation(num_list.begin(), num_list.end())); //모든 순열을 구함
+
+
+    cout << max_num;
+
+}
+
