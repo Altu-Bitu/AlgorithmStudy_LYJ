349cb783a9d2fdb6cdef344ff09aa00d5fc06db2
YujeongLEE <dldbwjd24@ewhain.net>
2021-10-12 15:24:53 +0900

    [그리디 알고리즘] 10월 12일 - Update
    1080번_피드백 반영

--- 10월 1일 - 그리디 알고리즘/1080.cpp
+++ 10월 1일 - 그리디 알고리즘/1080.cpp
@@ -17,8 +17,10 @@ void change(int x, int y) {
         for (int j = 0; j < 3; j++) {
             // i~ i+3-1
             // j~ j+3-1
-            if (arr[x + i][y + j] == 0) arr[x + i][y + j] = 1;
-            else arr[x + i][y + j] = 0;
+            // 한줄로 -> 삼항연산자 사용?
+            arr[x + i][y + j] == 0 ? arr[x + i][y + j] = 1 : arr[x + i][y + j] = 0;
+//            if (arr[x + i][y + j] == 0) arr[x + i][y + j] = 1;
+//            else arr[x + i][y + j] = 0;
 
         }
     }
@@ -31,29 +33,47 @@ int main() {
     int n, m;
     cin >> n >> m;
 
+
+    string s;
     vector<int> tmp(m, 0);
 
-    int temp;
-    cin.ignore();
-
     for (int i = 0; i < n; i++) {
+        cin >> s;
         for (int j = 0; j < m; j++) {
-            temp = cin.get(); //문자 1개를 입력받음
-            tmp[j] = temp - '0'; //문자 - '0' 숫자로 변환 해서 저장
+            tmp[j] = s[j] - '0';
         }
-        cin.ignore();//엔터무시
         arr.push_back(tmp);
     }
 
     for (int i = 0; i < n; i++) {
+        cin >> s;
         for (int j = 0; j < m; j++) {
-            temp = cin.get(); //문자 1개를 입력받음
-            tmp[j] = temp - '0'; //문자 - '0' 숫자로 변환 해서 저장
+            tmp[j] = s[j] - '0';
         }
-        cin.ignore();//엔터무시
         ans.push_back(tmp);
     }
 
+// 이 방법보다는 위처럼 string에 저장해서 숫자로 변환하기!
+//    int temp;
+//    cin.ignore();
+//
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < m; j++) {
+//            temp = cin.get(); //문자 1개를 입력받음
+//            tmp[j] = temp - '0'; //문자 - '0' 숫자로 변환 해서 저장
+//        }
+//        cin.ignore();//엔터무시
+//        arr.push_back(tmp);
+//    }
+//
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < m; j++) {
+//            temp = cin.get(); //문자 1개를 입력받음
+//            tmp[j] = temp - '0'; //문자 - '0' 숫자로 변환 해서 저장
+//        }
+//        cin.ignore();//엔터무시
+//        ans.push_back(tmp);
+//    }
 
 
 
