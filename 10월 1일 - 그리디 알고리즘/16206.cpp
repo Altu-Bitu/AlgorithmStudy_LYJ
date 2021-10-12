ea7149033a09e238f0a2e53cdb8af70d88b85644
YujeongLEE <dldbwjd24@ewhain.net>
2021-10-12 15:59:39 +0900

    [그리디 알고리즘] 10월 12일 - Update
    16206번

--- 10월 1일 - 그리디 알고리즘/16206.cpp
+++ 10월 1일 - 그리디 알고리즘/16206.cpp
@@ -8,20 +8,38 @@
 
 using namespace std;
 
-vector<int> cakes;
+priority_queue<int, vector<int>, greater<int>> cakes;
 priority_queue<int, vector<int>, greater<int>> ten_cakes;
+int ans = 0; //길이 10짜리 시나몬롤
+int lol;
 
+void cutCakes(priority_queue<int, vector<int>, greater<int>> &q) {
+
+    int cake = q.top();
+    q.pop();
+
+    //cut
+    ans++; //자른 앞부분은 크기 10됨
+    lol = cake - 10;//잘린 뒷부분
+
+//    if (lol < 10) return;//10이하인건 무시
+
+    if (lol == 10) ans++;
+    else if (lol % 10 == 0) ten_cakes.push(lol);
+    else if (lol >= 10)cakes.push(lol);
+
+}
 
 int main() {
 
 
-    int ans = 0; //길이 10짜리 시나몬롤
+//    int ans = 0; //길이 10짜리 시나몬롤
 
     int n, m;
     cin >> n >> m;
 
 
-    int lol;
+//    int lol;
     while (n--) {
 
         cin >> lol;
@@ -30,7 +48,7 @@ int main() {
 
         if (lol == 10) ans++;
         else if (lol % 10 == 0) ten_cakes.push(lol);
-        else cakes.push_back(lol);
+        else cakes.push(lol);
 
     }
 
@@ -39,29 +57,34 @@ int main() {
 
 
         if (!ten_cakes.empty()) {
-            int cake = ten_cakes.top();
-            ten_cakes.pop();
 
-            //cut
-            ans++; //자른 앞부분은 크기 10됨
-            lol = cake - 10;//잘린 뒷부분
-
-            if (lol < 10) continue;//10이하인건 무시
-
-            if (lol == 10) ans++;
-            else if (lol % 10 == 0) ten_cakes.push(lol);
-            else cakes.push_back(lol);
+            cutCakes(ten_cakes);
+//            int cake = ten_cakes.top();
+//            ten_cakes.pop();
+//
+//            //cut
+//            ans++; //자른 앞부분은 크기 10됨
+//            lol = cake - 10;//잘린 뒷부분
+//
+//            if (lol < 10) continue;//10이하인건 무시
+//
+//            if (lol == 10) ans++;
+//            else if (lol % 10 == 0) ten_cakes.push(lol);
+//            else if (lol >= 10)cakes.push(lol);
 
         } else if (!cakes.empty()) {
 
-            int cake = cakes[0];
-            cakes.erase(cakes.begin());//맨앞원소 삭제
+            cutCakes(cakes);
 
-            //cut
-            ans++; //자른 앞부분은 크기 10됨
-            lol = cake - 10;//잘린 뒷부분
-
-            if (lol >= 10) cakes.push_back(lol);//10이하인건 무시
+//            int cake = cakes[0];
+//            cakes.erase(cakes.begin());//맨앞원소 삭제
+//
+//
+//            //cut
+//            ans++; //자른 앞부분은 크기 10됨
+//            lol = cake - 10;//잘린 뒷부분
+//
+//            if (lol >= 10) cakes.push_back(lol);//10이하인건 무시
 //
 //            if (lol == 10) ans++;
 //            else if (lol % 10 == 0) ten_cakes.push(lol);
