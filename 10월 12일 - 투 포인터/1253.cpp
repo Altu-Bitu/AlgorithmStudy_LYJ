22f0ed652817d2fa692a0e5308e893613714f593
YujeongLEE <dldbwjd24@ewhain.net>
2021-10-18 16:52:14 +0900

    [투 포인터] 10월 18일 - ing
    1253번

--- 10월 12일 - 투 포인터/1253.cpp
+++ 10월 12일 - 투 포인터/1253.cpp
@@ -1,6 +1,8 @@
 //
 // 1253번 - 좋다
 //
+// 참고 : https://www.acmicpc.net/board/view/68348
+//
 
 #include <iostream>
 #include <vector>
@@ -25,19 +27,28 @@ int main() {
     sort(number.begin(), number.end());
 
     int ans = 0;
-    for (int i = 2; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         int target = number[i];
 
-//        cout << "target number : " << target << "\n";
 
 
-        int left = 0, right = i - 1;
+        int left = 0, right = n - 1; // right = i-1가 안되는 이유 : 음수고려하지 않았음
         int sum = number[left] + number[right];
         while (left < right) {
 
-//            cout << "left : " << left << " right: " << right << "\n";
-//            cout << "left number : " << number[left] << " right: " << number[right] << "\n";
-//            cout << "sum : " << sum << "\n";
+
+            if (right == i) {
+                right--;
+                sum -= number[right + 1];
+                sum += number[right];
+                continue;
+            }
+            if (left == i) {
+                left++;
+                sum -= number[left - 1];
+                sum += number[left];
+                continue;
+            }
 
 
             if (sum == target) {
@@ -53,7 +64,11 @@ int main() {
                 sum += number[left];
             }
 
+
+
         }
+
+
 
     }
 
