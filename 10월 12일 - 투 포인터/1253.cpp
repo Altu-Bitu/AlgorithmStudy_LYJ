615473d1ae1c55d1d27673c6a586caf4f920228f
YujeongLEE <dldbwjd24@ewhain.net>
2021-10-18 16:54:53 +0900

    [투 포인터] 10월 18일 - ing
    1253번_주석

--- 10월 12일 - 투 포인터/1253.cpp
+++ 10월 12일 - 투 포인터/1253.cpp
@@ -23,13 +23,12 @@ int main() {
         cin >> number[i];
     }
 
-
+    //투포인터를 사용하기 위해 정렬 필요
     sort(number.begin(), number.end());
 
-    int ans = 0;
-    for (int i = 0; i < n; i++) {
+    int ans = 0;//GOOD 한 수의 갯수
+    for (int i = 0; i < n; i++) { // 0부터 시작해야하는 반례 3개 0 0 0
         int target = number[i];
-
 
 
         int left = 0, right = n - 1; // right = i-1가 안되는 이유 : 음수고려하지 않았음
@@ -37,6 +36,7 @@ int main() {
         while (left < right) {
 
 
+            //본인은 제외되어야 함
             if (right == i) {
                 right--;
                 sum -= number[right + 1];
@@ -65,9 +65,7 @@ int main() {
             }
 
 
-
         }
-
 
 
     }
