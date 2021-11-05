a99e6a6d5222181a8340b53eabfded1c42c4ce11
YujeongLEE <dldbwjd24@ewhain.net>
2021-11-05 23:46:02 +0900

    [최단 경로] 11월 5일 - ing
    1063번

--- 11월 02일 - 최단 경로 /1063.cpp
+++ 11월 02일 - 최단 경로 /1063.cpp
@@ -39,84 +39,62 @@ int main() {
 
     cin >> king >> stone >> n;
 
-    //first : row , second : col
+    //first : row , second : col (A = 1)
     ii king_pos = make_pair(king[1] - '0', king[0] - 'A' + 1);
     ii stone_pos = make_pair(stone[1] - '0', stone[0] - 'A' + 1);
 
-//    cout << "king pos : " << king_pos.first << " " << king_pos.second;
-//    cout << "stone pos : " << stone_pos.first << " " << stone_pos.second;
 
     string move;
     while (n--) {
         cin >> move;
         ii dir = findDirection(move);
 
-//        cout << "==========================================================\n";
-//        cout << "======================START===================\n";
-//        cout << "==========================================================\n";
-//        cout << "king pos : " << king_pos.first << " " << king_pos.second << "\n";
-//        cout << "stone pos : " << stone_pos.first << " " << stone_pos.second << "\n";
-
-
+        //stone과 king의 위치관계 파악
         int row = king_pos.first - stone_pos.first;
         int col = king_pos.second - stone_pos.second;
-//        cout << "==========================================================\n";
-//        cout << "row : " << row << " col : " << col << "\n";
 
-//주의 1 . 차이가 1이어야함 ( 이동했을 때 돌과 "같은"위치에 존재할때만 돌을 옮기는 것)
+        //주의 1 . 이동했을 때 돌과 "같은"위치에 존재할때만 돌을 옮기는 것 -> 차이 범위 -1~1
         string state = "NONE";
         if (col == 0) { //같은 col에 존재
             if (row == 1)state = "B";
             else if (row == -1) state = "T";
-//            state = row > 0 ? "B" : "T";
-        } else if (row == 0) {
+        } else if (row == 0) { // 같은 row 에 존재
             if (col == 1)state = "L";
             else if (col == -1) state = "R";
-//            state = col > 0 ? "L" : "R";
-        } else if (abs(col) == abs(row)) {
+        } else if (abs(col) == 1 && abs(row) == 1) { //대각선 경로 어딘가
             if (col == 1) {//LEFT
                 if (row == 1)state = "LB";
                 else if (row == -1) state = "LT";
-//                state = row > 0 ? "LB" : "LT";
             } else if (col == -1) {
                 if (row == 1)state = "RB";
                 else if (row == -1) state = "RT";
             }
-//                state = row > 0 ? "RB" : "RT";
         }
 
-//        cout << "==========================================================\n";
-//        cout << "state : " << state << "\n";
 
+        if (state == move) { //돌과 방향이 같음 -> 돌이 이동하기 위한 조건
 
-        if (state == move) { //돌과 방향이 같음
-
-//            ii new_king = make_pair(king_pos.first + dir.first, king_pos.second + dir.second);
+            //돌을 이동시킴
             ii new_stone = make_pair(stone_pos.first + dir.first, stone_pos.second + dir.second);
-//            cout << "==========================================================\n";
-//            cout << "king new pos : " << new_king.first << " " << new_king.second << "\n";
-//            cout << "stone new pos : " << new_stone.first << " " << new_stone.second << "\n";
+
+            //범위 내에 있을 때만 이동
             if (new_stone.first > 0 && new_stone.first < 9 && new_stone.second > 0 && new_stone.second < 9) {
-//                king_pos = new_king;
                 stone_pos = new_stone;
             }
 
 
         }
-//        else {
 
+        //킹은 항상 이동
         ii new_king = make_pair(king_pos.first + dir.first, king_pos.second + dir.second);
-//            cout << "==========================================================\n";
-//            cout << "king new pos : " << new_king.first << " " << new_king.second << "\n";
 
+        //범위 내에 존재 && 돌과 겹칠수는 없음
         if (new_king != stone_pos && new_king.first > 0 && new_king.first < 9 && new_king.second > 0 &&
             new_king.second < 9)
             king_pos = new_king;
-
-//        }
-
     }
 
+    //col부터 출력 (알파벳으로 변환)
     cout << char(king_pos.second + 'A' - 1) << king_pos.first << "\n";
     cout << char(stone_pos.second + 'A' - 1) << stone_pos.first;
 
