faf2f67b72ef80ed5bab4af0e5b65e0a6843d9cb
YujeongLEE <dldbwjd24@ewhain.net>
2021-11-08 18:00:09 +0900

    [투 포인터] 11월 8일 - Update
    1253번

--- 10월 12일 - 투 포인터/1253.cpp
+++ 10월 12일 - 투 포인터/1253.cpp
@@ -12,6 +12,37 @@ using namespace std;
 
 vector<int> number;
 
+//배열 내 서로 다른 두 수를 더했을 때, target이 되는 수가 존재하는가
+bool findGoodNum(int target, int n, int i) {
+    int left = 0, right = n - 1; // right = i-1가 안되는 이유 : 음수고려하지 않았음
+    int sum;
+    while (left < right) {
+
+        sum = number[left] + number[right];
+
+        //본인은 제외되어야 함
+        if (right == i) {
+            right--;
+            continue;
+        }
+        if (left == i) {
+            left++;
+            continue;
+        }
+
+
+        if (sum == target) {
+            return true;// 존재하므로 true 반환
+        } else if (sum > target) {
+            right--;
+        } else if (sum < target) {
+            left++;
+
+        }
+    }
+
+    return false;//검사가 종료되었는데 발견되지 않음 -> false
+}
 
 int main() {
 
@@ -30,43 +61,8 @@ int main() {
     for (int i = 0; i < n; i++) { // 0부터 시작해야하는 반례 3개 0 0 0
         int target = number[i];
 
-
-        int left = 0, right = n - 1; // right = i-1가 안되는 이유 : 음수고려하지 않았음
-        int sum = number[left] + number[right];
-        while (left < right) {
-
-
-            //본인은 제외되어야 함
-            if (right == i) {
-                right--;
-                sum -= number[right + 1];
-                sum += number[right];
-                continue;
-            }
-            if (left == i) {
-                left++;
-                sum -= number[left - 1];
-                sum += number[left];
-                continue;
-            }
-
-
-            if (sum == target) {
-                ans++;
-                break;
-            } else if (sum > target) {
-                right--;
-                sum -= number[right + 1];
-                sum += number[right];
-            } else if (sum < target) {
-                left++;
-                sum -= number[left - 1];
-                sum += number[left];
-            }
-
-
-        }
-
+        bool res = findGoodNum(target, n, i);
+        if (res) ans++; //true가 return된 경우 정답의 수를 증가
 
     }
 
