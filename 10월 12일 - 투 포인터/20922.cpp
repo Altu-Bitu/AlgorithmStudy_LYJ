72aeeb69ba9a9346bf0f8bf57986b4c9a883400d
YujeongLEE <dldbwjd24@ewhain.net>
2021-11-09 16:29:48 +0900

    [투 포인터] 11월 9일 - Update
    20922번

--- 10월 12일 - 투 포인터/20922.cpp
+++ 10월 12일 - 투 포인터/20922.cpp
@@ -20,56 +20,28 @@ int main() {
         cin >> v[i];
     }
 
-    int max = 1;
+    int max = 0;
 
-    map<int, int> m;
+    map<int, int> m; //등장 수, 등장 횟수
 
-    int left = 0, right = 1;
-    m[v[left]]++;
-    int size = 1; //지금까지의 길이
-    int flag = false; // false : right로 움직이기 / true:left로 움직이기
-    int max_cnt = 100001;//절대 나올일 없는 숫자
-
-    while (left <= right && right < v.size()) {
-
-        int num;
-
-        if (flag) {//left가 움직인 경우 ->백스탭
-            num = v[left - 1]; //주의 : 이전 수를 없앤 것임
-            m[num]--;
-            size--;
-
-        } else {//right가 움직인 경우 -> 전진
-            num = v[right];
-            m[num]++;
-            size++;
-
-            if (m[num] > k) {
-                max_cnt = num; // 가장 등장 횟수가 많은 수를 저장해둠
-            }
+    int left = 0, right = 0;
 
 
-        }
+    while (left <= right && right < n) {
 
-
-        if (m[max_cnt] > k) { //한계도달 ( right쪽  그만 탐색하고 왼쪽으로 줄여나가기 )
-            flag = true;
-            if (size > max) {
-                max = size - 1; //길이가 더 긴 경우 답 갱신
-            }
+        // right를 증가시키고 종료했으므로 =이 붙어야함
+        if (m[v[right]] >= k) { //한계도달 ( right쪽  그만 탐색하고 왼쪽으로 줄여나가기 )
+            m[v[left]]--;
             left++;
-
         } else {//한계 미도달 (right쪽으로 계속 탐색)
-            flag = false;
-            right++;
+            m[v[right]]++;
+            if (right - left + 1 > max) {
+                max = right - left + 1; //길이가 더 긴 경우 답 갱신
+            }
+            right++; //right를 증가시키고 종료
         }
-
-
     }
 
-    if (size > max) { //마지막 체크
-        max = size;
-    }
 
     cout << max;
 
