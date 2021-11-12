35994f38e70815bfb4dd79356df5291b7a61d52e
YujeongLEE <dldbwjd24@ewhain.net>
2021-11-13 00:38:50 +0900

    [동적 계획법과 최단 거리 역추적] 11월 13일 - ing

---
+++ 11월 09일 - 동적 계획법과 최단 거리 역추적/2615.cpp
@@ -0,0 +1,25 @@
+//
+// 2615번 - 오목
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    vector<vector<int>> board(19, vector<int>(19, 0));
+
+    for (int i = 0; i < 19; i++) {
+        for (int j = 0; j < 19; j++) {
+            cin >> board[i][j];
+
+            if(board[i][j]==1)
+            else if(board[i][j]==2)
+        }
+    }
+
+
+
+}
