e8b0699c0a01d972fb1034f5ff5a3f8006e677b6
YujeongLEE <dldbwjd24@ewhain.net>
2021-11-29 04:30:20 +0900

    [유니온 파인드] 11월 29일 - ing
    1043번_에러

--- 11월 23일 - 유니온 파인드/1043.cpp
+++ 11월 23일 - 유니온 파인드/1043.cpp
@@ -5,27 +5,35 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <map>
 
 using namespace std;
 
 vector<int> parent;
+vector<bool> truth;
 
-
-int find(int node) {
+int findParent(int node) {
 
     if (parent[node] < 0)
         return node;
-    return parent[node] = find(parent[node]);
+    return parent[node] = findParent(parent[node]);
 
 }
 
 void unionInput(int x, int y) {
 
     //각각의 부모
-    int xp = find(x);
-    int yp = find(y);
+    int xp = findParent(x);
+    int yp = findParent(y);
 
-    if (xp == yp) return;//같은집합이면 합칠 수 없음
+
+    if (xp == yp)
+        return;
+
+    if (truth[x] || truth[y]) {//둘중에 한명이라도 진실을 알면
+        truth[xp] = truth[yp] = true;//부모도 진실을 알게됨
+    }
+
 
     if (parent[xp] < parent[yp]) { //더 작은게 자식이 많음(루트가 됨) -> 자식의 수가 음수로 저장되어있어서
         parent[xp] += parent[yp]; //yp의 자식도 xp가 가지게됨
@@ -45,98 +53,40 @@ int main() {
 
     int n, m; //사람의 수, 파티의 수
     cin >> n >> m;
-    parent.assign(n + 1, -1);//음수로 초기
-
+    parent.assign(n + 1, -1);//음수로 초기화
+    truth.assign(n + 1, false);
 
     int knower, a, b;
 
-    //진실을 아는 사람들을 하나의 집합으로 묶음
+
     cin >> knower;//진실을 아는 사람의 수
     for (int i = 0; i < knower; i++) {
-        if (i == 0) {//가장 처음에 입력된 사람
-            cin >> a;
-        } else {
-            cin >> b;
-            unionInput(a, b);
-        }
+        cin >> a;
+        truth[a] = true;//진실을 아는 사람들을 표시해둠
     }
 
 
-    int ans = 0;
-    int num, p;
+    int num;
+    int k = 1;
+    int ans = m; //과장된 이야기를 할 수 있는 파티의 수
     while (m--) {
-        bool flag = false; //진실을 아는사람이 존재하는가
-        bool flag2 = false; //과장된 이야기를 아는사람이 존재하는가
-        cin >> num;//파티 참여자 수
-        queue<int> participant;
-        while (num--) {
-            cin >> p;
-
-            if (find(p) == find(a)) {//참여자가 진실을 아는 사람
-                flag = true;
-                continue;
-            }
-
-            if (find(p) == find(0)) {//과장된 이야기를 알고 있는 사람
-                flag2 = true;
-                continue;
-            }
-            /*
-             * 집합이 없는 자 -> 상관없음 (루트가 본인임)
-             * 집합이 있는 자 ( 진실을 아는 자 / 과장된 이야기를 아는 자 ) -> 둘이 공존하면 안됨
-             *
-             * 진실을 아는 자가 존재함 -> 구라불가
-             * 과장된 이야기를 아는 자가 존재함 -> 구라 가능
-             * 진실을 아는 자와 과장된 이야기를 아는 자가 존재함 -> 구라불가
-             * 진실을 아는사람, 과장된 이야기를 아는 사람 둘다 없다 -> 구라가능
-             *
-             *
-             */
-
-            participant.push(p);//진실도, 과장된 이야기도 모르는 사람이면 큐에 넣기
+        cin >> num;
+        cin >> a;
+        if (truth[findParent(a)]) {//진실을 아는사람이 있으면
+            ans--;//과장된 이야기 불가능
+            
         }
 
-        cout << "BEFORE=============\n";
-        for (int i = 0; i < n + 1; i++) {
-            cout << parent[i] << " ";
-        }
-        cout << "\n";
-
-        if (flag && flag2) {
-            ans--;//구라를 알아차리는 사람이 생겨남!!
-            cout << "구라불가 \n";
-            while (!participant.empty()) {
-                int ps = participant.front();
-                participant.pop();
-                unionInput(a, ps);
-                cout << "ps 's parent : " << find(ps) << "\n";
-            }
-        } else if (flag && !flag2) {
-            cout << "구라불가 진실만 얘기해야함\n";
-            while (!participant.empty()) {
-                int ps = participant.front();
-                participant.pop();
-                unionInput(a, ps);
-                cout << "ps 's parent : " << find(ps) << "\n";
-            }
-        } else {
-            cout << "구라가능\n";
-            ans++;
-            while (!participant.empty()) {
-                int ps = participant.front();
-                participant.pop();
-                unionInput(0, ps);//어쩌피 0번사람 없으니까 여기다 넣자!
-                cout << "ps 's parent : " << find(ps) << "\n";
+        for (int i = 1; i < num; i++) {
+            cin >> b;
+            unionInput(a, b);
+            if (truth[findParent(b)]) {
+                ans--;
+                cout << k << "파티\n";
+                break;
             }
         }
-
-        cout << "AFTER=============\n";
-        for (int i = 0; i < n + 1; i++) {
-            cout << parent[i] << " ";
-        }
-        cout << "\n";
-
-
+        k++;
     }
 
     cout << ans;
