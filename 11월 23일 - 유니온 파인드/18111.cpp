594db0fe3067d9e29f9ff771f9ff3b4e8f2797f9
YujeongLEE <dldbwjd24@ewhain.net>
2021-12-03 15:08:03 +0900

    [유니온 파인드] 12월 3일 - Update
    18111번

--- 11월 23일 - 유니온 파인드/18111.cpp
+++ 11월 23일 - 유니온 파인드/18111.cpp
@@ -7,7 +7,7 @@
 #include <map>
 #include <algorithm>
 
-#define MAX 3e8
+#define MAX 7e7 //최대 500*256 + 6.4*10^7 개
 using namespace std;
 
 vector<vector<int>> mapp;
@@ -22,11 +22,11 @@ bool cmp(pair<int, int> &a, pair<int, int> &b) {
 
 
 //블럭 보유상황(?)
-int isPossible(vector<pair<int, int>> &v, long long b, int height) {
+int isPossible(vector<pair<int, int>> &v, int b, int height) {
 
-    long long add = 0, sub = 0; //필요한 블럭의 수
-    long long time = 0;
-    long long diff;
+    int add = 0, sub = 0; //필요한 블럭의 수
+//    int time = 0;
+    int diff;
     for (int i = 0; i < v.size(); i++) {
         if (v[i].first == height) continue;//높이가 같은 애들은 패스
 
@@ -44,21 +44,21 @@ int isPossible(vector<pair<int, int>> &v, long long b, int height) {
 
     if (b + add < sub) { //블럭 수가 부족해서 불가능함
         return MAX;
-    } else {//시간을 구할 수 있음
-        time = add * 2 + sub * 1;
     }
-    return time;
+
+
+    return add * 2 + sub * 1;//블럭의 수가 충분하여 시간을 구하면됨
 
 }
 
 
 int main() {
 
-    long long min = MAX;
-    long long max = -1;
+    int min = MAX;
+    int max = -1;
 
     int n, m;
-    long long b;
+    int b;
     cin >> n >> m >> b;
 
     mapp.assign(n, vector<int>(m, 0));
@@ -93,9 +93,11 @@ int main() {
         if (res < ans.first) {
             ans.first = res;
             ans.second = h;
-        } else if (res == ans.first && h > ans.second) {//길이가 같을떈 더 높은 것
-            ans.second = h;
         }
+// max부터 값을 줄여가면서 진행 + <=이 아니므로 h에 더 높은값이 자동으로 저장될 수 있음
+//        else if (res == ans.first && h > ans.second) {//길이가 같을떈 더 높은 것
+//            ans.second = h;
+//        }
 
     }
 
