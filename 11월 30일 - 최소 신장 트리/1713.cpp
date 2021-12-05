63cc49004d157f3f0603303843fe52941a4f7e2c
YujeongLEE <dldbwjd24@ewhain.net>
2021-12-06 01:08:28 +0900

    [최소 신장 트리] 12월 6일 - ing
    1713번_25%

--- 11월 30일 - 최소 신장 트리/1713.cpp
+++ 11월 30일 - 최소 신장 트리/1713.cpp
@@ -5,9 +5,11 @@
 #include <iostream>
 #include <tuple>
 #include <vector>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
+typedef tuple<int, int, int> tp;
 
 vector<pair<int, int>> vote;
 
@@ -31,20 +33,34 @@ vector<pair<int, int>> vote;
  *
  */
 
+struct compare {
+    bool operator()(const tp &a, const tp &b) {
 
-int leastVote(int min_pick, int pick) {//더 적은 인간을 리턴함
+        if (get<1>(a) != get<1>(b))
+            return get<1>(a) > get<1>(b);//투표수 적은게 앞
+
+        return get<2>(a) > get<2>(b);//더 오래된(작은수)사진이 앞
+    }
+};
 
 
-    //매번 교체될 사람을 갱신해보기
-    //가장 적은 표를 받은 학생을 항상 저장해두기
-    if (vote[min_pick].first > vote[pick].first) {//표수가 더적으면 갱신
-        return pick;
-    } else if (vote[min_pick].first == vote[pick].first && vote[min_pick].second > vote[pick].second) {
-        return pick;
+int leastPick() {//더 적은 인간을 리턴함
+
+    priority_queue<tp, vector<tp>, compare> pq;
+
+    for (int i = 1; i <= 100; i++) {
+
+        if (vote[i].second == -1) continue;//거론된적도 없는 사람
+        pq.push({i, vote[i].first, vote[i].second});
+
     }
 
-    return min_pick;
+//    while (!pq.empty()) {
+//        cout << get<0>(pq.top()) << "\n";
+//        pq.pop();
+//    }
 
+    return get<0>(pq.top()); //퇴출되어야 하는 후보
 
 }
 
@@ -54,75 +70,60 @@ int main() {
     int n, m; //사진틀, 학생의 추천횟수
     cin >> n >> m;
 
-    vote.assign(21, {m + 1, m + 1});
+    vote.assign(101, {0, -1});
 
-    int pick, cnt = 0, min_pick = -1;
+    int pick, cnt = 0;
 
 
     for (int i = 0; i < m; i++) {
         cin >> pick;
 
-        if (i == 0) min_pick = pick;
-
-        cout << i << "회차\n";
-
-        //매번 교체될 사람을 갱신해보기
-        //가장 적은 표를 받은 학생을 항상 저장해두기
-        if (vote[pick].first != m + 1 && vote[min_pick].first > vote[pick].first) {//표수가 더적으면 갱신
-            min_pick = pick;
-        } else if (vote[min_pick].first == vote[pick].first && vote[min_pick].second > vote[pick].second) {
-            //표수는 같은데, 더오래된애 있으면 갱신
-            min_pick = pick;
-        }
-
-
-        cout << "min pick : " << min_pick << "\n";
 
         //해당 후보가 이미 존재하는 후보인지 확인
-        if (vote[pick].first != m + 1) {
-            cout << pick << "은 이미 존재하는 후보입니다.\n";
+        if (vote[pick].first != 0) {
+//            cout << pick << "은 이미 존재하는 후보입니다.\n";
             vote[pick].first++;
             cnt++;
+
             continue;
         }
 
-
+        //존재하는 후보가 아님
         if (cnt >= n) {//사진 칸이 다참
-            cout << "사진 칸이 전부 차있다\n";
-            cout << min_pick << "\n";
+//            cout << "사진 칸이 전부 차있다\n";
+
+            //교체될 사람을 찾아내기
+            int min_pick = leastPick();
+
+//            cout << min_pick << "후보를 내쫓는다\n";
             //교체될 사람을 그냥 초기화 해버리기
-            vote[min_pick].first = m + 1;
-            vote[min_pick].second = m + 1;
+            vote[min_pick].first = 0;
+            vote[min_pick].second = -1;
 
         }
 
 
-        cout << "새로운 사람을 추가한다\n";
+//        cout << "새로운 사람 " << pick << "을 추가한다\n";
         //새로운 사람을 넣기
         vote[pick].first++; //투표수 증가
         vote[pick].second = i;// 처음언급된 시간을 표시
         cnt++;
 
 
-        for (int j = 0; j < 21; j++) {
-            cout << " [ " << j << " ] " << vote[j].first << " " << vote[j].second << "\n";
-        }
-
-
-/*
- * 2 7 6
- * 2 1 1
- *
- */
-
+//        for (int j = 0; j < 101; j++) {
+//            cout << " [ " << j << " ] " << vote[j].first << " " << vote[j].second << "\n";
+//        }
 
 
     }
 
-
-//    for (int i = 0; i < 21; i++) {
-//        cout << " [ " << i << " ] " << vote[i].first << " " << vote[i].second << "\n";
+//    for (int j = 0; j < 101; j++) {
+//        cout << " [ " << j << " ] " << vote[j].first << " " << vote[j].second << "\n";
 //    }
+
+    for (int i = 0; i < 101; i++) {
+        if (vote[i].second != -1) cout << i << " ";
+    }
 
 
 }
