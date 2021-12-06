69eeb35caf2239228a24c546c0cbd7ca09a77912
YujeongLEE <dldbwjd24@ewhain.net>
2021-12-06 21:43:38 +0900

    [최소 신장 트리] 12월 6일 - Update
    1713번

--- 11월 30일 - 최소 신장 트리/1713.cpp
+++ 11월 30일 - 최소 신장 트리/1713.cpp
@@ -55,11 +55,6 @@ int leastPick() {//더 적은 인간을 리턴함
 
     }
 
-//    while (!pq.empty()) {
-//        cout << get<0>(pq.top()) << "\n";
-//        pq.pop();
-//    }
-
     return get<0>(pq.top()); //퇴출되어야 하는 후보
 
 }
@@ -70,7 +65,7 @@ int main() {
     int n, m; //사진틀, 학생의 추천횟수
     cin >> n >> m;
 
-    vote.assign(101, {0, -1});
+    vote.assign(101, {0, -1});//총 추천받을 수 있는 학생 1-100
 
     int pick, cnt = 0;
 
@@ -81,7 +76,6 @@ int main() {
 
         //해당 후보가 이미 존재하는 후보인지 확인
         if (vote[pick].first != 0) {
-//            cout << pick << "은 이미 존재하는 후보입니다.\n";
             vote[pick].first++;
 
             continue;
@@ -89,36 +83,23 @@ int main() {
 
         //존재하는 후보가 아님
         if (cnt >= n) {//사진 칸이 다참
-//            cout << "사진 칸이 전부 차있다\n";
 
             //교체될 사람을 찾아내기
             int min_pick = leastPick();
 
-//            cout << min_pick << "후보를 내쫓는다\n";
-            //교체될 사람을 그냥 초기화 해버리기
+            //교체될 사람을 사진칸에서 완전히 제거
             vote[min_pick].first = 0;
             vote[min_pick].second = -1;
 
         }
 
-
-//        cout << "새로운 사람 " << pick << "을 추가한다\n";
         //새로운 사람을 넣기
         vote[pick].first++; //투표수 증가
         vote[pick].second = i;// 처음언급된 시간을 표시
         cnt++;
 
 
-//        for (int j = 0; j < 101; j++) {
-//            cout << " [ " << j << " ] " << vote[j].first << " " << vote[j].second << "\n";
-//        }
-
-
     }
-
-//    for (int j = 0; j < 101; j++) {
-//        cout << " [ " << j << " ] " << vote[j].first << " " << vote[j].second << "\n";
-//    }
 
     for (int i = 0; i < 101; i++) {
         if (vote[i].second != -1) cout << i << " ";
