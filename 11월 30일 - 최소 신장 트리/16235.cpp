0fdd0410e1b1eeb75f8c234f3930c08a546aac7f
YujeongLEE <dldbwjd24@ewhain.net>
2021-12-06 22:27:09 +0900

    [최소 신장 트리] 12월 6일 - Update
    16235번_deque사용

--- 11월 30일 - 최소 신장 트리/16235.cpp
+++ 11월 30일 - 최소 신장 트리/16235.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <tuple>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 typedef tuple<int, int, int> tp;
@@ -24,8 +25,10 @@ int main() {
     vector<vector<int>> feed(n, vector<int>(n, 0));//겨울 양분
     vector<vector<int>> ground(n, vector<int>(n, 5)); //땅
 
+    deque<tp> tree;// pq는 시간초과 이므로 deque를 이용해 1회만 정렬 (어쩌피 새로 추가되는 나무는 나이가 제일 적다) 
 
-    priority_queue<tp, vector<tp>, greater<>> tree;
+//    priority_queue<tp, vector<tp>, greater<>> tree;
+
 
     queue<tp> tree_tmp;
     queue<tp> die;
@@ -39,23 +42,26 @@ int main() {
     int a, b, c;
     while (m--) {
         cin >> a >> b >> c;
-        tree.push({c, a - 1, b - 1}); //나무를 저장 (위치, 나이) -> 나이어린순으로 priority queue
+        tree.push_back({c, a - 1, b - 1}); //나무를 저장 (위치, 나이) -> 나이어린순으로 priority queue
     }
+
+    sort(tree.begin(), tree.end(), greater<tp>());// 나이어린걸 뒤로 priority queue
+
 
     while (k--) {
 
         //봄
-        while (!tree.empty()) {//모든 나무에대해
-            int age = get<0>(tree.top());
-            int x = get<1>(tree.top());
-            int y = get<2>(tree.top());
-            tree.pop();
+        while (!tree.empty()) {//모든 나무에대해 (어린나무(뒷쪽부터)
+            int age = get<0>(tree.back());
+            int x = get<1>(tree.back());
+            int y = get<2>(tree.back());
+            tree.pop_back();
 
 
             if (ground[x][y] >= age) {//양분을 먹을 수 있음
                 ground[x][y] -= age;//양분을 먹음
                 age++;//나이가 증가
-                tree_tmp.push({age, x, y});
+                tree_tmp.push({age, x, y});//어린나무가 앞쪽에 쌓임(?)
             } else {
                 //양분을 먹을 수 없음 -> 죽기
                 die.push({age, x, y});
@@ -78,11 +84,13 @@ int main() {
         int row[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
         int col[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
-        while (!tree_tmp.empty()) {
+        while (!tree_tmp.empty()) {//나이적은 나무 부터 꺼내짐
             int age = get<0>(tree_tmp.front());
             int x = get<1>(tree_tmp.front());
             int y = get<2>(tree_tmp.front());
             tree_tmp.pop();
+
+            tree.push_front({age, x, y});//쓴 나무 버리면 안됨(앞쪽으로 넣어주기)
 
             if (age % 5 == 0) {//나이가 5의배수
                 for (int i = 0; i < 8; i++) {
@@ -90,13 +98,12 @@ int main() {
                     int nc = y + col[i];
 
                     if (nr >= 0 && nr < n && nc >= 0 && nc < n) {
-                        tree.push({1, nr, nc});//해당 위치에 나이가 1살인 나무가 생김
+                        tree.push_back({1, nr, nc});//해당 위치에 나이가 1살인 나무가 생김 -> 1은 어쩌피 제일 작은 수 이므로 뒤에 넣어준다
                     }
 
                 }
             }
 
-            tree.push({age, x, y});//쓴 나무 버리면 안됨
 
         }
 
