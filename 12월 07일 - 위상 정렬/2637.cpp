ec9f142081477fe0dded7c9597cf43c5b65bc738
YujeongLEE <dldbwjd24@ewhain.net>
2021-12-20 20:42:56 +0900

    [위상 정렬] 12월 20일 - ing
    2637번

--- 12월 07일 - 위상 정렬/2637.cpp
+++ 12월 07일 - 위상 정렬/2637.cpp
@@ -12,7 +12,7 @@ using namespace std;
 typedef pair<int, int> ii;
 
 //위상정렬
-vector<vector<int>> topologicalSort(int n, vector<int> &indegree, vector<vector<ii>> &graph) {
+vector<int> topologicalSort(int n, int start, vector<int> &indegree, vector<vector<ii>> &graph) {
     vector<int> result;
     queue<ii> q;
 
@@ -23,12 +23,13 @@ vector<vector<int>> topologicalSort(int n, vector<int> &indegree, vector<vector<
             q.push({i, i});
     }
 
+//    q.push({start, start});
 
-    vector<vector<int>> dp(q.size() + 1, vector<int>(n + 1, 0));
-    for (int i = 1; i <= q.size(); i++) {
-        dp[i][i] = 1;
-    }
-
+    vector<int> dp(n + 1, 0);
+    dp[start] = 1;
+//    for (int i = 1; i <= q.size(); i++) {
+//        dp[i][i] = 1;
+//    }
 
     while (!q.empty()) {
         int node = q.front().first;
@@ -36,13 +37,20 @@ vector<vector<int>> topologicalSort(int n, vector<int> &indegree, vector<vector<
         q.pop();
 
         result.push_back(node); //형재 정점 순서에 삽입
+//        cout << "============================\n";
+//        cout << "\n지금의 노드 : " << node;
+//        cout << "\n지금의 노드의 부모(?) : " << pre_node;
         for (int i = 0; i < graph[node].size(); i++) {
             int next_node = graph[node][i].first;
+//            cout << "\n지금의 노드와 연결된 노드 " << i << "번 : " << next_node;
             indegree[next_node]--; //연결된 정점의 진입차수를 1씩 감소
             if (!indegree[next_node]) //연결된 정점의 진입차수가 0이 되었다면
                 q.push({next_node, pre_node});
 
-            dp[pre_node][next_node] += graph[node][i].second * dp[pre_node][node];//second (몇개 필요한지)
+//            cout << "\ndp[" << pre_node << "][" << next_node << "] += graph[" << node << "][" << i << "].second * dp["
+//                 << pre_node << "][" << node << "]\n";
+            dp[next_node] += graph[node][i].second * dp[node];//second (몇개 필요한지)
+//            cout << dp[next_node] << " += " << graph[node][i].second << " * " << dp[node] << "\n";
         }
     }
 
@@ -71,22 +79,36 @@ int main() {
 
     }
 
+    for (int i = 1; i <= n; i++) {
+        if (indegree[i] == 0) { //진입차수가 0이라면
+            vector<int> idg = indegree;
+            cout << " start : " << i << "\n";
+            vector<int> res = topologicalSort(n, i, idg, graph);
 
-    vector<vector<int>> res = topologicalSort(n, indegree, graph);
+            for (int j = 0; j < res.size(); j++) {
+                cout << res[j] << " ";
+            }
 
+            cout << "\n";
 
-    for (int i = 0; i < res.size(); i++) {
-
-
-        for (int j = 0; j < res[i].size(); j++) {
-            cout << res[i][j] << " ";
         }
-
-        cout << "\n";
-
     }
 
-    cout << "\n";
+//    vector<vector<int>> res = topologicalSort(n, indegree, graph);
+
+//    cout << "\n";
+//    for (int i = 0; i < res.size(); i++) {
+//
+//
+//        for (int j = 0; j < res[i].size(); j++) {
+//            cout << res[i][j] << " ";
+//        }
+//
+//        cout << "\n";
+//
+//    }
+//
+//    cout << "\n";
 
 //    for (int i = 0; i < res1.size(); i++) {
 //        cout << res1[i].second << " ";
